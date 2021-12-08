f71223d95ad92fb7029b0f3de34e203cd6b1ddca
TFlippy <TFlippy@users.noreply.github.com>
2021-12-08 10:01:52 +0100

    Sludge particles

--- content/Entities/Resources/Sludge.cs
+++ content/Entities/Resources/Sludge.cs
@@ -12,29 +12,32 @@
 
 #if CLIENT
 		[ISystem.LateUpdate(ISystem.Mode.Single)]
-		public static void OnUpdateAnimation(ISystem.Info info, [Source.Owned] in Transform.Data transform, [Source.Owned] ref Sludge.Data sludge, [Source.Owned] in Resource.Data resource)
+		public static void UpdateFX(ISystem.Info info, [Source.Owned] in Transform.Data transform, [Source.Owned] ref Sludge.Data sludge, [Source.Owned] in Resource.Data resource)
 		{
-			ref var material = ref resource.material.GetDefinition();
-			var modifier = resource.quantity / material.quantity_max;
-
 			if (info.WorldTime >= sludge.next_smoke)
 			{
 				var random = XorRandom.New();
 
+				ref var region = ref info.GetRegion();
+				ref var material = ref resource.material.GetDefinition();
+
+				var modifier = MathF.Log2(1.00f + (resource.quantity / MathF.Max(material.quantity_max, 1.00f)));
+				var modifier2 = 0.50f + (modifier * 0.50f);
+
 				sludge.next_smoke = info.WorldTime + random.NextFloatRange(0.70f, 1.30f);
 
-				Particle.Spawn(ref info.GetRegion(), new Particle.Data()
+				Particle.Spawn(ref region, new Particle.Data()
 				{
 					texture = texture_smoke,
-					lifetime = random.NextFloatRange(2.00f, 6.00f),
-					pos = transform.position + random.NextVector2(0.50f),
+					lifetime = random.NextFloatRange(2.00f, 6.00f) * modifier2,
+					pos = transform.position - new Vector2(0.00f, -0.50f),
 					vel = new Vector2(0.10f, -0.10f) * 1.50f,
 					force = new Vector2(0.14f, -0.50f) * random.NextFloatRange(0.90f, 1.10f),
 					fps = (byte)random.NextFloatRange(12, 14),
 					frame_count = 64,
 					frame_count_total = 64,
 					frame_offset = (byte)random.NextFloatRange(0, 64),
-					scale = random.NextFloatRange(0.40f, 0.50f),
+					scale = random.NextFloatRange(0.40f, 0.50f) * modifier2,
 					rotation = random.NextFloat(10.00f),
 					angular_velocity = random.NextFloat(1.20f),
 					growth = 0.20f,
