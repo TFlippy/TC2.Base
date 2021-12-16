f8c7056e12bbc1a6b5676685a4801112ae0a1e37
Wunarg <warl.durm@gmail.com>
2021-12-16 12:53:24 +0100

    Jump Force adjustment and scaling

--- content/Entities/Creatures/Runner.cs
+++ content/Entities/Creatures/Runner.cs
@@ -20,7 +20,7 @@ namespace TC2.Base.Components
 		{
 			public float walk_force;
 			public float jump_force;
-			public float max_speed = 15.00f;
+			public float max_speed = 13.00f;
 
 			public float force_modifier = 1.00f;
 			public float speed_modifier = 1.00f;
@@ -37,7 +37,7 @@ namespace TC2.Base.Components
 		public static void UpdateOrganic(ISystem.Info info, Entity entity,
 		[Source.Owned] ref Runner.Data runner, [Source.Owned] in Organic.Data organic)
 		{
-			runner.force_modifier = organic.strength;
+			runner.force_modifier = MathF.Pow(organic.strength, 0.50f);
 			runner.speed_modifier = organic.efficiency;
 		}
 
@@ -48,13 +48,13 @@ namespace TC2.Base.Components
 			ref readonly var keyboard = ref control.keyboard;
 
 			var force = new Vector2(0, 0);
-			var vel = body.GetVelocity();
+			Vector2 velocity = body.GetVelocity();
 
-			//Walking and Air Movement direction TODO: use two different values for custom usage
+			//Walking and Air Movement direction TODO: use two different values for custom usage instead of multiplier
 			if (!keyboard.GetKey(Keyboard.Key.NoMove))
 			{
-				if (keyboard.GetKey(Keyboard.Key.MoveLeft) && vel.X > -runner.max_speed) force.X -= runner.walk_force;
-				if (keyboard.GetKey(Keyboard.Key.MoveRight) && vel.X < +runner.max_speed) force.X += runner.walk_force;
+				if (keyboard.GetKey(Keyboard.Key.MoveLeft)) force.X -= runner.walk_force;
+				if (keyboard.GetKey(Keyboard.Key.MoveRight)) force.X += runner.walk_force;
 			}
 
 			if (!keyboard.GetKey(Keyboard.Key.NoMove) && keyboard.GetKey(Keyboard.Key.MoveLeft | Keyboard.Key.MoveRight)) runner.flags |= Runner.Flags.Walking;
@@ -88,7 +88,7 @@ namespace TC2.Base.Components
 
 					var dot = Vector2.Dot(normal, new Vector2(MathF.Sign(normal.X), 0));
 
-					force.Y = -MathF.Abs(dot * force.X) * friction * 0.50f;
+					force.Y -= MathF.Abs(dot * force.X) * (1.00f - friction) * 0.50f; //friction IMPROVED climbing
 					dot = MathF.Max(0.00f, dot - 0.20f); //Slight bumps are ignored
 					force.X *= (1.00f - dot/2.00f);
 				}
@@ -97,7 +97,6 @@ namespace TC2.Base.Components
 			//Slowed walking while in the air, also no jumping
 			if (arbiter_count <= 0 && (info.WorldTime - runner.last_ground) > 0.20f)
 			{
-				
 				force.X *= 0.50f;
 				force.Y *= 0.00f;
 				runner.flags &= ~Runner.Flags.Grounded;
@@ -107,13 +106,18 @@ namespace TC2.Base.Components
 			//JUMP, Coyotee time 0.2s, Jump cooldown 0.4s (Static numbers)
 			if (!keyboard.GetKey(Keyboard.Key.NoMove) && keyboard.GetKey(Keyboard.Key.MoveUp) && (info.WorldTime - runner.last_jump) > 0.40f && (info.WorldTime - runner.last_ground) < 0.20f)
 			{
-				runner.jump_force_current = runner.jump_force;
+				runner.jump_force_current = (runner.jump_force - MathF.Abs(velocity.Y * body.GetMass() * App.tickrate) * 0.25f);
 				runner.last_jump = info.WorldTime;
+			}
+			if (!keyboard.GetKey(Keyboard.Key.MoveUp))
+			{
+				runner.jump_force_current *= 0.10f;
 			}
 
 			//Half jump force each tick
 			force.Y -= runner.jump_force_current;
 			runner.jump_force_current *= 0.50f;
+			//runner.jump_force_current = 0.00f;
 
 			//CROUCH
 			if (!keyboard.GetKey(Keyboard.Key.NoMove) && control.keyboard.GetKey(Keyboard.Key.MoveDown) && (info.WorldTime - runner.last_ground) <= 0.25f) runner.flags |= Runner.Flags.Crouching;
@@ -121,6 +125,7 @@ namespace TC2.Base.Components
 
 			var max_speed = new Vector2(runner.max_speed, 10);
 
+			//Crouching reduced current jump force and max speed
 			if (runner.flags.HasAll(Runner.Flags.Crouching)) //TODO: Set max speed to a specific value instead of multiplying
 			{
 				runner.jump_force_current *= 0.50f;
@@ -130,33 +135,27 @@ namespace TC2.Base.Components
 
 			//STOP //TODO: use specialized stopping force
 
-			Vector2 velocity = body.GetVelocity();
-
+			if (runner.flags.HasAll(Runner.Flags.Grounded))
 			if (!runner.flags.HasAll(Runner.Flags.Walking) && (runner.jump_force_current <= 1.00f))
 			{
 				var required_force_dir = (velocity * body.GetMass() * App.tickrate) - force;
 				required_force_dir = required_force_dir.GetNormalized(out var required_force_magnitude);
 				required_force_dir *= Maths.Clamp(runner.walk_force * 2.00f, -required_force_magnitude, required_force_magnitude);
 
-				if (!runner.flags.HasAll(Runner.Flags.Grounded)) //No stopping while not on the ground
-				{
-					required_force_dir.X *= 0.02f;
-					required_force_dir.Y *= 0.00f;
-				}
-
 				force -= required_force_dir;
 			}
 
-			//VELOCITY SPEED REDUCTION (the faster you go the less force your legs add, currently linear)
+			//VELOCITY SPEED REDUCTION (the faster you go the less force your legs add, currently linear up to max speed)
 			if ((velocity.X > 0.00f && force.X > 0.00f) || (velocity.X < 0.00f && force.X < 0.00f))
 			{
 				force.X *= 1.00f - MathF.Abs(velocity.X / max_speed.X);
 			}
 
 			max_speed *= runner.speed_modifier;
-			force *= runner.force_modifier;
+			force.X *= runner.force_modifier;
+			force.Y *= MathF.Pow(runner.force_modifier, 0.60f); //Reduced jump scaling with strength
 
-			//force = Physics.LimitForce(ref body, force, max_speed);
+			//force = Physics.LimitForce(ref body, force, max_speed); //No longer relevant
 			body.AddForce(force);
 		}
 	}
