c90d27204a8a808702d783fd8e326e805320de67
Wunarg <warl.durm@gmail.com>
2021-12-22 18:17:11 +0100

    New Modification file

--- content/Modifications/Modifications.cs
+++ content/Modifications/Modifications.cs
@@ -419,35 +419,6 @@ namespace TC2.Base
 
 			definitions.Add(Modification.Definition.New<Body.Data>
 			(
-				identifier: "body.random_activation",
-				category: "Utility",
-				name: "Random Activation",
-				description: "Item randomly activates on its own.",
-
-				// This randomly causes a left click and a space bar (at the same time)
-				// Working examples: guns, fuse explosives, drills, mounts (yes they will use whatever is on them), melee weapons, even medkits
-				// Due to the wide variety of uses this has, this costs a large amount of materials
-				// This doesn't aim, so using anything which uses aim direction requires additional setup
-
-				apply_0: static (ref Modification.Context context, ref Body.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
-				{
-					ref var random_activation = ref context.GetOrAddComponent<RandomActivation.Data>();
-					random_activation.duration += 0.20f;
-				},
-
-				apply_1: static (ref Modification.Context context, ref Body.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
-				{
-					context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // High cost
-
-					if (!context.GetComponent<Melee.Data>().IsNull())
-					{
-						context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // Even higher cost on melee weapons
-					}
-				}
-			));
-
-			definitions.Add(Modification.Definition.New<Body.Data>
-			(
 				identifier: "body.bulk",
 				name: "Batch Production",
 				description: "More efficient manufacturing process by producing multiple items in bulk.",
