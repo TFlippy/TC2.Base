0da0794a641b07e868c5c1d0fdb29495a86504ef
Wunarg <warl.durm@gmail.com>
2022-01-08 16:17:44 +0100

    Mindswap (not working)

---
+++ Content/Modifications/Components/Mindswap.cs
@@ -0,0 +1,58 @@
+﻿
+namespace TC2.Base.Components
+{
+	public static partial class Mindswap
+	{
+		[Flags]
+		public enum Flags: uint
+		{
+			None = 0,
+
+			Active = 1 << 0
+		}
+
+		[IComponent.Data(Net.SendType.Reliable)]
+		public partial struct Data: IComponent
+		{
+			public Mindswap.Flags flags;
+		}
+
+
+		[ISystem.EarlyUpdate(ISystem.Mode.Single), Exclude<Toolbelt.Data>(Source.Modifier.Parent)]
+		public static void Update(ISystem.Info info, Entity entity,
+		[Source.Owned] ref Control.Data control, [Source.Owned] ref Mindswap.Data mindswap)
+		{
+			if (!mindswap.flags.HasFlag(Flags.Active))
+			{
+				Entity parent = entity.GetParent(Relation.Type.Child);
+				if (!parent.IsNull() && !(parent == 0))
+				{
+					App.WriteLine(parent+ "A");
+					Entity next = parent.GetParent(Relation.Type.Instance);
+					bool stop = false;
+					float counter = 0;
+					while (!next.IsNull() && !(next == 0) && !stop)
+					{
+						parent = next;
+						App.WriteLine(parent + "B" + counter);
+						counter++;
+						next = parent.GetParent(Relation.Type.Instance);
+
+						ref var character = ref parent.GetComponent<Character.Data>();
+						if (!character.IsNull()) 
+						{
+							stop = true;
+#if SERVER
+							App.WriteLine(parent + "C");
+
+							character.ent_controlled.SetCharacter(entity);
+							mindswap.flags |= Flags.Active;
+							entity.SyncComponent(ref mindswap);
+#endif
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- Content/Modifications/Modifications.Control.cs
+++ Content/Modifications/Modifications.Control.cs
@@ -34,6 +34,69 @@ namespace TC2.Base
 					}
 				}
 			));
+			definitions.Add(Modification.Definition.New<Control.Data>
+			(
+				identifier: "control.mind_swap",
+				category: "Control",
+				name: "Mind Swap",
+				description: "Swap your mind with this when holding it.",
+
+				// This randomly causes a left click and a space bar (at the same time)
+				// Working examples: guns, fuse explosives, drills, mounts (yes they will use whatever is on them), melee weapons, even medkits
+				// Due to the wide variety of uses this has, this costs a large amount of materials
+				// This doesn't aim, so using anything which uses aim direction requires additional setup
+
+				apply_0: static (ref Modification.Context context, ref Control.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var mindswap = ref context.GetOrAddComponent<Mindswap.Data>();
+					ref var organic = ref context.GetOrAddComponent<Organic.Data>();
+					organic.tags |= Organic.Tags.Brain;
+					ref var organicstate = ref context.GetOrAddComponent<Organic.State>();
+					organicstate.consciousness_shared = 1.00f;
+					ref var npc = ref context.GetOrAddComponent<NPC.Data>();
+				},
+
+				apply_1: static (ref Modification.Context context, ref Control.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // High cost
+
+					if (!context.GetComponent<Melee.Data>().IsNull())
+					{
+						context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // Even higher cost on melee weapons
+					}
+				}
+			));
+			/*definitions.Add(Modification.Definition.New<Control.Data>
+			(
+				identifier: "control.auto_aim",
+				category: "Control",
+				name: "Auto Aim",
+				description: "Tries to automaticly aim towards nearby other creatures.",
+
+				// This randomly causes a left click and a space bar (at the same time)
+				// Working examples: guns, fuse explosives, drills, mounts (yes they will use whatever is on them), melee weapons, even medkits
+				// Due to the wide variety of uses this has, this costs a large amount of materials
+				// This doesn't aim, so using anything which uses aim direction requires additional setup
+
+				apply_0: static (ref Modification.Context context, ref Control.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var npc = ref context.GetOrAddComponent<NPC.Data>();
+					
+					npc.navigation_state = NPC.NavigationState.Searching;
+					npc.find_distance_max = 10.00f;
+					npc.lose_distance_max = 128.00f;
+				},
+
+				apply_1: static (ref Modification.Context context, ref Control.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // High cost
+
+					if (!context.GetComponent<Melee.Data>().IsNull())
+					{
+						context.requirements_new.Add(Crafting.Requirement.Resource("salt.motion", 10.00f)); // Even higher cost on melee weapons
+					}
+				}
+			));*/
 		}
 	}
 }
