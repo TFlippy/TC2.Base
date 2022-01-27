1418e86d87b3cce975a3c507ff976aa611d60c28
TFlippy <TFlippy@users.noreply.github.com>
2022-01-27 18:56:18 +0100

    Supplier stuff

--- content/Entities/Supplier.cs
+++ content/Entities/Supplier.cs
@@ -39,6 +39,26 @@ namespace TC2.Base.Components
 			new(40.00f, new(Shipment.Item.Resource("iron_plate", 10.00f, 20.00f), step: 10.00f))
 		};
 
+		public static readonly WeightedList<Supplier.Product> products_creatures = new()
+		{
+			new(40.00f, new(Shipment.Item.Resource("leather", 5.00f, 50.00f), step: 1.00f)),
+			new(37.00f, new(Shipment.Item.Resource("cloth", 5.00f, 50.00f), step: 1.00f)),
+			new(30.00f, new(Shipment.Item.Resource("chitin", 5.00f, 50.00f), step: 1.00f)),
+			new(25.00f, new(Shipment.Item.Resource("meat", 20.00f, 150.00f), step: 1.00f)),
+			new(10.00f, new(Shipment.Item.Resource("insect", 10.00f, 100.00f), step: 1.00f)),
+			new(5.00f, new(Shipment.Item.Resource("red_sugar", 1.00f, 25.00f), step: 1.00f)),
+		};
+
+		public static readonly WeightedList<Supplier.Product> products_mushrooms = new()
+		{
+			new(100.00f, new(Shipment.Item.Resource("mushroom", 20.00f, 250.00f), step: 1.00f)),
+			new(80.00f, new(Shipment.Item.Resource("mushroom.brown", 10.00f, 200.00f), step: 1.00f)),
+			new(70.00f, new(Shipment.Item.Resource("mushroom.red", 20.00f, 200.00f), step: 1.00f)),
+			new(50.00f, new(Shipment.Item.Resource("mushroom.yellow", 10.00f, 150.00f), step: 1.00f)),
+			new(20.00f, new(Shipment.Item.Resource("mushroom.green", 5.00f, 100.00f), step: 1.00f)),
+			new(5.00f, new(Shipment.Item.Resource("mushroom.blue", 1.00f, 50.00f), step: 1.00f)),
+		};
+
 #if SERVER
 		[ISystem.LateUpdate(ISystem.Mode.Single)]
 		public static void OnLateUpdate(ISystem.Info info, Entity entity,
@@ -60,12 +80,15 @@ namespace TC2.Base.Components
 					var items = shipment.items.AsSpan();
 
 					var random = XorRandom.New();
+					
+					var count = random.NextIntRange(2, 4);
+					var bonus_amount_multiplier = 1.00f + random.NextFloatRange(0.00f, (4 - count) * 0.50f);
 
-					for (int i = 0; i < 4; i++)
+					for (int i = 0; i < count; i++)
 					{
-						var product = products.GetRandom(ref random);
+						var product = products_mushrooms.GetRandom(ref random);
 						var item = product.item;
-						item.quantity = (random.NextFloatRange(item.quantity, item.max) * supplier.amount_multiplier).ToNearestMultiple(product.step);
+						item.quantity = (random.NextFloatRange(item.quantity, item.max) * supplier.amount_multiplier * bonus_amount_multiplier).ToNearestMultiple(product.step);
 
 						items.Add(item);
 					}
