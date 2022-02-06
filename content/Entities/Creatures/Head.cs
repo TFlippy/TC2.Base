26277f95cf0a3a3be4753ad9f15238f14600e0b3
TFlippy <TFlippy@users.noreply.github.com>
2022-02-06 03:32:10 +0100

    Head sounds

--- content/Entities/Creatures/Head.cs
+++ content/Entities/Creatures/Head.cs
@@ -18,7 +18,40 @@ namespace TC2.Base.Components
 		public partial struct Data: IComponent
 		{
 			public float voice_pitch = 1.00f;
+
+			public Sound.Handle sound_death;
+			public Sound.Handle sound_hit;
+			public Sound.Handle sound_pain;
+			public Sound.Handle sound_attack;
+
+			[Save.Ignore, Net.Ignore] public float next_pain;
 		}
+
+#if SERVER
+		//[ISystem.Add(ISystem.Mode.Single), HasTag("dead", true, Source.Modifier.Owned)]
+		//public static void OnDeath(ISystem.Info info, [Source.Owned] in Transform.Data transform, [Source.Owned] in Head.Data head)
+		//{
+		//	Sound.Play(ref info.GetRegion(), head.sound_death, transform.position);
+		//}
+
+		[ISystem.VeryLateUpdate(ISystem.Mode.Single), HasTag("dead", false, Source.Modifier.Owned)]
+		public static void UpdateGroan(ISystem.Info info, [Source.Owned] ref Organic.Data organic, [Source.Owned] ref Organic.State organic_state, [Source.Owned] ref Head.Data head, [Source.Owned] in Transform.Data transform)
+		{
+			if (info.WorldTime > head.next_pain)
+			{
+				//App.WriteLine(organic_state.pain_shared);
+				if (organic_state.pain_shared > 500.00f && organic_state.consciousness_shared > 0.30f)
+				{
+#if SERVER
+					ref var region = ref info.GetRegion();
+					var random = XorRandom.New();
+					Sound.Play(ref region, head.sound_pain, transform.position, volume: 0.20f + Maths.Clamp(organic_state.pain_shared / 2000.00f, 0.20f, 0.50f), pitch: random.NextFloatRange(0.70f, 1.10f) * (1.00f + Maths.Clamp((organic_state.pain_shared_new / 2000.00f), 0.00f, 0.10f)) * head.voice_pitch);
+					head.next_pain = info.WorldTime + (Maths.Clamp(10.00f - (organic_state.pain_shared / 300.00f), 5.00f, 10.00f) * random.NextFloatRange(0.80f, 1.30f));
+#endif
+				}
+			}
+		}
+#endif
 
 		[ISystem.Update(ISystem.Mode.Single)]
 		public static void UpdateNoRotate(ISystem.Info info, [Source.Owned] in Organic.Data organic, [Source.Owned] in Organic.State organic_state, [Source.Owned] ref NoRotate.Data no_rotate, [Source.Owned] in Head.Data head)
