b6cf5cc78c89e9e8877615ecfc358581d212ed90
TFlippy <TFlippy@users.noreply.github.com>
2022-04-06 20:39:49 +0200

    Added Paxilon

--- content/Modifications/Modifications.cs
+++ content/Modifications/Modifications.cs
@@ -744,6 +744,51 @@ namespace TC2.Base
 				}
 			));
 
+			definitions.Add(Modification.Definition.New<Consumable.Data>
+			(
+				identifier: "consumable.paxilon",
+				category: "Consumable",
+				name: "Mix: Paxilon Hydrochlorate",
+				description: "Mixes a dose of paxilon hydrochlorate into the item.",
+
+				can_add: static (ref Modification.Context context, in Consumable.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					return !context.HasComponent<Paxilon.Effect>();
+				},
+
+				validate: static (ref Modification.Context context, in Consumable.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var value = ref handle.GetData<float>();
+					value = Maths.Clamp(value, 1.00f, 100.00f);
+
+					return true;
+				},
+
+#if CLIENT
+				draw_editor: static (ref Modification.Context context, in Consumable.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var value = ref handle.GetData<float>();
+					return GUI.SliderFloat("Amount", ref value, 1.00f, 100.00f, "%.2f");
+				},
+#endif
+
+				apply_0: static (ref Modification.Context context, ref Consumable.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var amount = ref handle.GetData<float>();
+
+					ref var alcohol = ref context.GetOrAddComponent<Paxilon.Effect>();
+					alcohol.amount = amount;
+				},
+
+				apply_1: static (ref Modification.Context context, ref Consumable.Data data, ref Modification.Handle handle, Span<Modification.Handle> modifications) =>
+				{
+					ref var amount = ref handle.GetData<float>();
+
+					ref var material = ref Material.GetMaterial("paxilon");
+					context.requirements_new.Add(Crafting.Requirement.Resource(material.id, amount * 0.001f / material.mass_per_unit));
+				}
+			));
+
 			definitions.Add(Modification.Definition.New<Pill.Data>
 			(
 				identifier: "pill.extended_release",
