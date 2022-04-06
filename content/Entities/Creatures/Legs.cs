b04c67b9d087f4893026a84dcd95358423801556
TFlippy <TFlippy@users.noreply.github.com>
2022-04-06 08:53:10 +0200

    Improved NoRotate on organics

--- content/Entities/Creatures/Legs.cs
+++ content/Entities/Creatures/Legs.cs
@@ -27,7 +27,7 @@ namespace TC2.Base.Components
 		[ISystem.Update(ISystem.Mode.Single), HasTag("dead", false, Source.Modifier.Owned)]
 		public static void UpdateNoRotateAlive(ISystem.Info info, [Source.Owned] in Organic.Data organic, [Source.Owned] in Organic.State organic_state, [Source.Owned, Override] ref NoRotate.Data no_rotate, [Source.Owned] in Legs.Data legs)
 		{
-			no_rotate.multiplier = MathF.Round(organic_state.consciousness_shared) * organic_state.efficiency * Maths.Lerp(0.20f, 1.00f, organic.motorics * organic.motorics) * organic.coordination;
+			no_rotate.multiplier = MathF.Round(organic_state.consciousness_shared * organic_state.efficiency * Maths.Lerp(0.20f, 1.00f, organic.motorics * organic.motorics) * organic.coordination);
 			no_rotate.speed *= Maths.Lerp(0.20f, 1.00f, organic.motorics);
 			no_rotate.bias += (1.00f - organic.motorics.Clamp01()) * 0.15f;
 		}
