3c55bc61e199f80f1068f081bda3117c279c6693
TFlippy <TFlippy@users.noreply.github.com>
2022-06-07 17:40:02 +0200

    NPC fixes

--- content/Entities/Creatures/Badger/Badger.cs
+++ content/Entities/Creatures/Badger/Badger.cs
@@ -5,6 +5,13 @@
 		[IComponent.Data(Net.SendType.Reliable)]
 		public partial struct Data: IComponent
 		{
+			public float test;
+		}
+
+		// Hack, used to register "badger" tag for now
+		[ISystem.AddFirst(ISystem.Mode.Single), HasTag("badger", true, Source.Modifier.Owned)]
+		public static void UpdateBadger(ISystem.Info info, [Source.Owned] ref Badger.Data badger)
+		{
 
 		}
 	}
