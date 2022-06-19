91f20d95fed943061fb7f978599a98d53d8892d4
Wunarg <warl.durm@gmail.com>
2022-06-19 11:55:01 +0200

    Not takes melee data if you want it to

--- Content/Entities/Tools/ThrowingDamage.cs
+++ Content/Entities/Tools/ThrowingDamage.cs
@@ -8,6 +8,8 @@ namespace TC2.Base.Components
 		{
 			public float damage = default;
 
+			public bool OverrideWithMelee = true; //Overrides the stats of this with melee components stats on this item if one exists
+
 			[Save.Ignore, Net.Ignore] public float next_hit = default;
 			[Save.Ignore, Net.Ignore] public float last_attach = default;
 
@@ -60,11 +62,16 @@ namespace TC2.Base.Components
 				{
 
 					var damagetype = Damage.Type.Blunt;
-					
+					var basedamage = throwing.damage * 0.1f;
+
 					ref var melee = ref entity.GetComponent<Melee.Data>(); //Copies melee damage type if the item has one
-					if(!melee.IsNull())
+					if(!melee.IsNull() && throwing.OverrideWithMelee)
 					{
 						damagetype = melee.damage_type;
+						var random = XorRandom.New();
+						basedamage = melee.damage_base * 0.1f + random.NextFloatRange(0.00f, melee.damage_bonus) * 0.1f;
+						//This is multiplied by velocity so the 0.1 multiplier mitigates this and encourages high speed throwing attacks
+
 					}
 
 					foreach (var arbiter in body.GetArbiters())
@@ -75,13 +82,13 @@ namespace TC2.Base.Components
 						{
 							var material_type = arbiter.GetMaterial();
 							var vel = arbiter.GetVelocity() - body.GetVelocity();
-							var vel_sq = vel.LengthSquared();
+							var vel_sq = vel.Length();
 							hit = true;
 							App.WriteLine("speed" + vel + " " + vel_sq);
 							if (ent_hit.IsValid() && vel_sq > 2.00f)
 							{
 								entity.Hit(ent_hit, ent_hit, arbiter.GetPosition(), -arbiter.GetNormal(), arbiter.GetNormal(), 
-								damage: throwing.damage * vel_sq * 0.1f, target_material_type: material_type, damage_type: damagetype);
+								damage: basedamage * vel_sq, target_material_type: material_type, damage_type: damagetype);
 							}
 						}
 					}
