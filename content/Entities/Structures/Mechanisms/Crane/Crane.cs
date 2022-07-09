25552a4a25dc7dce1ad1bceaedbb909596c14773
TFlippy <TFlippy@users.noreply.github.com>
2022-07-09 23:22:08 +0200

    Attachment customization improvements

--- content/Entities/Structures/Mechanisms/Crane/Crane.cs
+++ content/Entities/Structures/Mechanisms/Crane/Crane.cs
@@ -1,4 +1,6 @@
 ﻿
+using static Keg.Engine.Game.Attachment;
+
 namespace TC2.Base.Components
 {
 	public static partial class Crane
@@ -8,7 +10,8 @@ namespace TC2.Base.Components
 		{
 			None = 0,
 
-			Inverted = 1 << 0
+			Inverted = 1 << 0,
+			Hold = 1 << 1,
 		}
 
 		[IComponent.Data(Net.SendType.Reliable), IComponent.With<Crane.State>]
@@ -17,7 +20,8 @@ namespace TC2.Base.Components
 			public float length_a = 8.00f;
 			public float length_b = 8.00f;
 
-			public Crane.Flags flags = Flags.None;
+			public Crane.Flags flags = Crane.Flags.None;
+			public Crane.Flags flags_editable = Crane.Flags.None;
 
 			public Data()
 			{
@@ -38,12 +42,14 @@ namespace TC2.Base.Components
 		{
 			public float length_a;
 			public float length_b;
+			public Crane.Flags flags;
 
 #if SERVER
 			public void Invoke(ref NetConnection connection, Entity entity, ref Crane.Data crane)
 			{
 				crane.length_a = this.length_a;
 				crane.length_b = this.length_b;
+				crane.flags = (crane.flags & ~crane.flags_editable) | (this.flags & crane.flags_editable);
 
 				entity.SyncComponent(ref crane);
 			}
@@ -89,14 +95,30 @@ namespace TC2.Base.Components
 						dirty |= GUI.SliderFloat("A", ref this.crane.length_a, 0.00f, 16.00f);
 						dirty |= GUI.SliderFloat("B", ref this.crane.length_b, 0.00f, 16.00f);
 
+						var flags = this.crane.flags;
+
+						var edit_rpc = new Crane.ConfigureRPC
+						{
+							length_a = this.crane.length_a,
+							length_b = this.crane.length_b,
+							flags = flags
+						};
+
+						if (this.crane.flags_editable.HasAny(Crane.Flags.Hold) && GUI.Checkbox("Hold", ref flags, Crane.Flags.Hold, new Vector2(96, 24)))
+						{
+							dirty = true;
+							edit_rpc.flags = flags;
+						}
+
+						if (this.crane.flags_editable.HasAny(Crane.Flags.Inverted) && GUI.Checkbox("Invert", ref flags, Crane.Flags.Inverted, new Vector2(96, 24)))
+						{
+							dirty = true;
+							edit_rpc.flags = flags;
+						}
+
 						if (dirty)
 						{
-							var rpc = new Crane.ConfigureRPC
-							{
-								length_a = this.crane.length_a,
-								length_b = this.crane.length_b,
-							};
-							rpc.Send(this.ent_crane);
+							edit_rpc.Send(this.ent_crane);
 						}
 					}
 				}
@@ -160,7 +182,7 @@ namespace TC2.Base.Components
 				//	transform_parent_tmp.scale.X *= -1.00f;
 				//}
 
-				//if (control.mouse.GetKey(Mouse.Key.Right))
+				if (!crane.flags.HasAny(Crane.Flags.Hold) || control.mouse.GetKey(Mouse.Key.Right))
 				{
 					IK.Resolve2x(new Vector2(crane.length_a, crane.length_b), transform_tmp.LocalToWorld(joint_base.offset_b), control.mouse.position, new(crane_state.angle_a, crane_state.angle_b), out var angles, invert: invert != crane.flags.HasAny(Crane.Flags.Inverted));
 					crane_state.angle_a = angles.X;
@@ -171,10 +193,12 @@ namespace TC2.Base.Components
 						crane_state.next_sync = info.WorldTime + 0.20f;
 						dirty |= true;
 					}
+
+					gear.rotation = crane_state.angle_b;
 				}
 
 				gear_parent.rotation = transform_parent_tmp.WorldToLocalRotationUnscaled(crane_state.angle_a);
-				gear.rotation = crane_state.angle_b;
+				//if (!crane.flags.HasAny(Crane.Flags.Hold) || control.mouse.GetKey(Mouse.Key.Right)) 
 
 				if (invert)
 				{
