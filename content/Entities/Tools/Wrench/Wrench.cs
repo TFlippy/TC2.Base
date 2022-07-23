50667103f12888ba121064557124be1833d19b10
TFlippy <TFlippy@users.noreply.github.com>
2022-07-23 16:21:55 +0200

    Wrench rework (WIP)

--- content/Entities/Tools/Wrench/Wrench.cs
+++ content/Entities/Tools/Wrench/Wrench.cs
@@ -1,4 +1,6 @@
 ﻿
+using static TC2.Base.Components.Wrench;
+
 namespace TC2.Base.Components
 {
 	public static partial class Wrench
@@ -6,15 +8,85 @@ namespace TC2.Base.Components
 		[IComponent.Data(Net.SendType.Reliable)]
 		public partial struct Data: IComponent
 		{
-			public int mode_index;
+			[Save.Ignore]
+			public ulong selected_component_id;
+		}
+
+		public static partial class Mode
+		{
+			public static partial class Belts
+			{
+				[IComponent.Data(Net.SendType.Reliable)]
+				public partial struct Data: IComponent, Wrench.IMode
+				{
+					[Save.Ignore] public Entity ent_src;
+					[Save.Ignore] public Entity ent_dst;
+
+					public static Sprite Icon { get; } = new Sprite("ui_icons_builder_categories", 0, 1, 16, 16, (uint)0, 0);
+
+#if CLIENT
+					public void Draw(Entity ent_wrench, ref Wrench.Data wrench)
+					{
+						GUI.Text("hi");
+					}
+#endif
+				}
+			}
 		}
 
 		public interface IMode: IComponent
 		{
-			
+			public static abstract Sprite Icon { get; }
+
+#if CLIENT
+			public void Draw(Entity ent_wrench, ref Wrench.Data wrench);
+#endif
+		}
+
+		public struct SelectModeRPC: Net.IRPC<Wrench.Data>
+		{
+			public ulong selected_component_id;
+
+#if SERVER
+			public void Invoke(ref NetConnection connection, Entity entity, ref Wrench.Data data)
+			{
+				if (entity.HasComponent(this.selected_component_id))
+				{
+					data.selected_component_id = this.selected_component_id;
+					data.Sync(entity);
+				}
+			}
+#endif
 		}
 
 #if CLIENT
+		public static void DrawModeButton<T>(Entity ent_wrench) where T : unmanaged, Wrench.IMode
+		{
+			ref var info = ref ECS.GetInfo<T>();
+
+			using (GUI.ID.Push(info.id))
+			{
+				if (GUI.DrawIconButton($"wrench.mode.{info.identifier}", T.Icon, new(40, 40)))
+				{
+					var rpc = new Wrench.SelectModeRPC()
+					{
+						selected_component_id = info.id
+					};
+					rpc.Send(ent_wrench);
+				}
+
+				if (GUI.IsItemHovered())
+				{
+					using (GUI.Tooltip.New())
+					{
+						GUI.Title(info.name);
+					}
+				}
+			}
+		}
+
+		public const string dock_identifier = "wrench.mode.dock";
+
 		public struct WrenchGUI: IGUICommand
 		{
 			public Entity ent_wrench;
@@ -35,24 +107,105 @@ namespace TC2.Base.Components
 
 						ref var region = ref Client.GetRegion();
 
-						GUI.Text("Derp");
+						GUI.Text($"Derp: {wrench.selected_component_id}");
+
+						using (GUI.Group.New(size: new(GUI.GetRemainingWidth(), 32)))
+						{
+							Wrench.DrawModeButton<Wrench.Mode.Belts.Data>(this.ent_wrench);
+							GUI.SameLine();
+
+							//var count = 4;
+							//for (var i = 0; i < count; i++)
+							//{
+							//	using (GUI.ID.Push(i + 1))
+							//	{
+							//		DrawModeButton<>
+
+							//		//if (GUI.DrawIconButton($"wrench.mode.{i}", new Sprite("ui_icons_builder_categories", 0, 1, 16, 16, (uint)i, 0), new(40, 40)))
+							//		//{
+
+							//		//}
+
+							//		//if (GUI.IsItemHovered())
+							//		//{
+							//		//	using (GUI.Tooltip.New())
+							//		//	{
+							//		//		//GUI.Title(category_names[i]);
+							//		//	}
+							//		//}
+
+							//		if (i < count - 1) GUI.SameLine();
+							//	}
+							//}
+						}
+
+						using (GUI.Group.New(size: GUI.GetRemainingSpace()))
+						{
+							GUI.Dock.New(Wrench.dock_identifier, size: GUI.GetRemainingSpace());
+							//var v = new WrenchModeGUI<Wrench.Mode.Belts.Data>();
+							//v.Draw();
+						}
+					}
+				}
+			}
+		}
+
+		public struct WrenchModeGUI<T>: IGUICommand where T : unmanaged, Wrench.IMode
+		{
+			public Entity ent_wrench;
+
+			public Transform.Data transform;
+			public Wrench.Data wrench;
+			public T mode;
+
+			public void Draw()
+			{
+				ref var info = ref ECS.GetInfo<T>();
+
+				using (var window = GUI.Window.Docked(info.identifier, dock_identifier: Wrench.dock_identifier, padding: new(8, 8)))
+				{
+					if (window.show)
+					{
+						GUI.Title(info.identifier);
+
+						this.mode.Draw(ent_wrench, ref this.wrench);
 					}
 				}
 			}
 		}
 
 		[ISystem.GUI(ISystem.Mode.Single)]
-		public static void OnGUI(ISystem.Info info, Entity entity, [Source.Parent] in Interactor.Data interactor, [Source.Owned] ref Wrench.Data wrench, [Source.Owned] in Transform.Data transform, [Source.Parent] in Player.Data player, [Source.Owned] in Control.Data control)
+		public static void OnGUI(ISystem.Info info, Entity entity, 
+		[Source.Parent] in Interactor.Data interactor, [Source.Owned] ref Wrench.Data wrench, 
+		[Source.Owned] in Transform.Data transform, [Source.Parent] in Player.Data player, [Source.Owned] in Control.Data control)
 		{
 			if (player.IsLocal())
 			{
+				//App.WriteLine("ye");
+
 				var gui = new WrenchGUI()
 				{
 					ent_wrench = entity,
 					transform = transform,
 					wrench = wrench
 				};
+				gui.Submit();
+			}
+		}
 
+		[ISystem.LateGUI(ISystem.Mode.Single)]
+		public static void OnGUIMode<T>(ISystem.Info info, Entity entity, 
+		[Source.Owned] in T mode, [Source.Parent] in Interactor.Data interactor, [Source.Owned] ref Wrench.Data wrench, 
+		[Source.Owned] in Transform.Data transform, [Source.Parent] in Player.Data player, [Source.Owned] in Control.Data control) where T : unmanaged, Wrench.IMode
+		{
+			if (player.IsLocal() && wrench.selected_component_id == ECS.GetID<T>())
+			{
+				var gui = new WrenchModeGUI<T>()
+				{
+					ent_wrench = entity,
+					transform = transform,
+					wrench = wrench
+				};
 				gui.Submit();
 			}
 		}
