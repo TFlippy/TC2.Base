d5eca4e4e983140974a8102cf062977e90ccc322
TFlippy <TFlippy@users.noreply.github.com>
2024-04-15 11:26:41 +0200

    Moved Architecture prefabs to TC2.Base

---
+++ content/Entities/Structures/Architecture/Cable/Cable.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TC2.Base.Components
+{
+	public static partial class Cable
+	{
+		[IComponent.Data(Net.SendType.Unreliable, region_only: true)]
+		public partial struct Data: IComponent
+		{
+			public float stability;
+
+			public Data()
+			{
+
+			}
+		}
+	}
+}
