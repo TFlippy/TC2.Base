721215f754c1bf875a3ff42e8e668c9b5f7ec9e3
TFlippy <TFlippy@users.noreply.github.com>
2024-05-19 14:26:12 +0200

    Updating trees

--- content/Entities/Nature/Tree.cs
+++ content/Entities/Nature/Tree.cs
@@ -32,7 +32,8 @@ namespace TC2.Base.Components
 
 #if SERVER
 		[ISystem.Modified<Split.Data>(ISystem.Mode.Single, ISystem.Scope.Region)]
-		public static void OnSplit(ISystem.Info info, Entity entity, [Source.Owned] in Split.Data split, [Source.Owned] in Tree.Data tree, [Source.Owned, Pair.Of<Tree.Data>] in Foliage.Renderer.Data renderer)
+		public static void OnSplit(ISystem.Info info, Entity entity, 
+		[Source.Owned] in Split.Data split, [Source.Owned] in Tree.Data tree, [Source.Owned, Pair.Of<Tree.Data>] in Foliage.Renderer.Data renderer)
 		{
 			if (split.rect_normalized.a.Y != 0.00f || split.rect_normalized.GetHeight() < 0.40f)
 			{
@@ -40,33 +41,33 @@ namespace TC2.Base.Components
 			}
 		}
 
-		[ISystem.Modified<Health.Data>(ISystem.Mode.Single, ISystem.Scope.Region)]
-		public static void OnModified(ISystem.Info info, Entity entity, ref XorRandom random, ref Region.Data region, 
-		[Source.Owned] in Health.Data health, [Source.Owned] ref Tree.Data tree, [Source.Owned] ref Body.Data body, [Source.Owned] in Transform.Data transform, [Source.Owned] ref Animated.Renderer.Data renderer)
-		{
-			if (health.integrity < tree.health_cut && tree.flags.TryAddFlag(Tree.Flags.Cut))
-			{
-				renderer.sprite = tree.sprite_cut;
+		//[ISystem.Modified<Health.Data>(ISystem.Mode.Single, ISystem.Scope.Region)]
+		//public static void OnModified(ISystem.Info info, Entity entity, ref XorRandom random, ref Region.Data region, 
+		//[Source.Owned] in Health.Data health, [Source.Owned] ref Tree.Data tree, [Source.Owned] ref Body.Data body, [Source.Owned] in Transform.Data transform, [Source.Owned] ref Animated.Renderer.Data renderer)
+		//{
+		//	if (health.integrity < tree.health_cut && tree.flags.TryAddFlag(Tree.Flags.Cut))
+		//	{
+		//		renderer.sprite = tree.sprite_cut;
 
-				Sound.Play(ref region, tree.sound_cut, transform.position);
-				Shake.Emit(ref region, transform.position, 0.40f, 0.40f);
+		//		Sound.Play(ref region, tree.sound_cut, transform.position);
+		//		Shake.Emit(ref region, transform.position, 0.40f, 0.40f);
 
-				//Experience.Add(health.last_damage_owner, Experience.Type.Woodcutting, (int)random.NextFloatRange(100, 150));
+		//		//Experience.Add(health.last_damage_owner, Experience.Type.Woodcutting, (int)random.NextFloatRange(100, 150));
 
-				body.type = Body.Type.Dynamic;
-				body.MarkDirty();
+		//		body.type = Body.Type.Dynamic;
+		//		body.MarkDirty();
 
-				region.SpawnPrefab(tree.prefab_stump, transform.position, transform.rotation, transform.scale);
+		//		region.SpawnPrefab(tree.prefab_stump, transform.position, transform.rotation, transform.scale);
 
-				//entity.MarkModified<Body.Data>();
+		//		//entity.MarkModified<Body.Data>();
 
-				//entity.RemoveTrait<Tree.Data, Foliage.Renderer.Data>();
+		//		//entity.RemoveTrait<Tree.Data, Foliage.Renderer.Data>();
 
-				renderer.Sync(entity, true);
-				body.Sync(entity, true);
-				tree.Sync(entity, true);
-			}
-		}
+		//		renderer.Sync(entity, true);
+		//		body.Sync(entity, true);
+		//		tree.Sync(entity, true);
+		//	}
+		//}
 #endif
 	}
 }
