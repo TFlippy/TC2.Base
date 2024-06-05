87c7aefc928b6e3c916c7137abb0ed01382183d3
TFlippy <TFlippy@users.noreply.github.com>
2024-06-05 17:42:38 +0200

    Tweaks

--- content/Entities/Structures/Mechanisms/Controllable.cs
+++ content/Entities/Structures/Mechanisms/Controllable.cs
@@ -14,6 +14,7 @@
 			}
 
 			[Asset.Ignore] public Vector2 target_pos;
+			public float speed = 1.00f;
 			public Controllable.Data.Flags flags;
 
 			[Net.Ignore, Save.Ignore] public float t_last_sync;
@@ -25,15 +26,15 @@
 			}
 		}
 
-//#if SERVER
-//		[ISystem.EarlyUpdate(ISystem.Mode.Single, ISystem.Scope.Region)]
-//		public static void UpdateJoint(ISystem.Info info, Entity entity, Entity ent_joint_base, 
-//		[Source.Shared] in Control.Data control, [Source.Shared] in Controllable.Data controllable, [Source.Shared] in Transform.Data transform,
-//		[Source.Owned] ref Joint.Base joint_base)
-//		{
-			
-//		}
-//#endif
+		//#if SERVER
+		//		[ISystem.EarlyUpdate(ISystem.Mode.Single, ISystem.Scope.Region)]
+		//		public static void UpdateJoint(ISystem.Info info, Entity entity, Entity ent_joint_base, 
+		//		[Source.Shared] in Control.Data control, [Source.Shared] in Controllable.Data controllable, [Source.Shared] in Transform.Data transform,
+		//		[Source.Owned] ref Joint.Base joint_base)
+		//		{
+
+		//		}
+		//#endif
 
 		[ISystem.EarlyUpdate(ISystem.Mode.Single, ISystem.Scope.Region)]
 		public static void UpdateControls(ISystem.Info info, Entity entity, Entity ent_joint_base,
@@ -42,11 +43,11 @@
 		{
 			if (controllable.flags.HasAny(Controllable.Data.Flags.Mouse_Orthogonal))
 			{
-				control.mouse.position = Maths.MoveTowards(control.mouse.position, controllable.target_pos, new Vector2(0.125f * 8));
+				control.mouse.position = Maths.MoveTowards(control.mouse.position, controllable.target_pos, new Vector2(controllable.speed * info.DeltaTime));
 			}
 			else
 			{
-
+				control.mouse.position = Maths.MoveTowards(control.mouse.position, controllable.target_pos, controllable.speed * info.DeltaTime);
 			}
 		}
 
@@ -68,6 +69,7 @@
 					sync |= this.mouse_position.TryCopyTo(ref data.target_pos);
 					sync |= this.keyboard.TryCopyTo(ref control.keyboard.GetMask());
 					sync |= this.mouse.TryCopyTo(ref control.mouse.GetMask());
+					sync |= this.flags.TryCopyTo(ref data.flags, Data.Flags.Mouse_Orthogonal);
 
 					if (sync)
 					{
@@ -105,6 +107,12 @@
 
 								var dirty = false;
 
+								var w_pos_mouse = this.control.mouse.position;
+								var w_pos_target = this.controllable.target_pos;
+
+								var c_pos_mouse = region.WorldToCanvas(w_pos_mouse);
+								var c_pos_target = region.WorldToCanvas(w_pos_target);
+
 								var rpc = new Controllable.ConfigureRPC
 								{
 
@@ -123,27 +131,40 @@
 
 								var picker_size = new Vector2(64, 64);
 
-								if (GUI.DrawButton("LMB", size: new Vector2((GUI.RmX * 0.50f) - (picker_size.X * 0.50f), 40), color: GUI.col_button_yellow))
+								using (var group_row = GUI.Group.New(size: new Vector2(GUI.RmX, 40)))
 								{
-									rpc.mouse.GetRefOrDefault().SetFlag(Mouse.Key.Left, true);
-									dirty = true;
+									if (GUI.DrawButton("LMB", size: new Vector2((GUI.RmX * 0.50f) - (picker_size.X * 0.50f), 40), color: GUI.col_button_yellow))
+									{
+										rpc.mouse.GetRefOrDefault().AddFlag(Mouse.Key.Left);
+										dirty = true;
+									}
+
+									GUI.SameLine();
+
+									var w_pos_target_tmp = w_pos_target;
+									if (GUI.Picker("mouse_pos", "Position", picker_size, ref w_pos_target_tmp, new Vector2(-4000, -4000), new Vector2(4000, 4000), sensitivity: 0.10f, absolute: true))
+									{
+										rpc.mouse_position = w_pos_target_tmp.SnapFloor(0.125f);
+										dirty = true;
+									}
+
+									GUI.SameLine();
+
+									if (GUI.DrawButton("RMB", size: new Vector2(GUI.RmX, 40), color: GUI.col_button_yellow))
+									{
+										rpc.mouse.GetRefOrDefault().AddFlag(Mouse.Key.Right);
+										dirty = true;
+									}
 								}
 
-								GUI.SameLine();
+								GUI.SeparatorThick();
 
-								var mouse_pos_tmp = this.control.mouse.position;
-								if (GUI.Picker("mouse_pos", "Position", picker_size, ref mouse_pos_tmp, new Vector2(-4000, -4000), new Vector2(4000, 4000), sensitivity: 0.10f, absolute: true))
+								using (var group_row = GUI.Group.New(size: new Vector2(GUI.RmX, 40)))
 								{
-									rpc.mouse_position = mouse_pos_tmp.SnapFloor(0.125f);
-									dirty = true;
-								}
-
-								GUI.SameLine();
-
-								if (GUI.DrawButton("RMB", size: new Vector2(GUI.RmX, 40), color: GUI.col_button_yellow))
-								{
-									rpc.mouse.GetRefOrDefault().SetFlag(Mouse.Key.Right, true);
-									dirty = true;
+									if (GUI.Checkbox("Orthogonal"u8, this.controllable.flags, ref rpc.flags, Controllable.Data.Flags.Mouse_Orthogonal, size: new(0, 24)))
+									{
+										dirty = true;
+									}
 								}
 
 
@@ -154,9 +175,21 @@
 									rpc.Send(this.ent_controllable);
 								}
 
+								var delta = w_pos_mouse - w_pos_target;
+								if (this.controllable.flags.HasAny(Data.Flags.Mouse_Orthogonal))
+								{
+
+								}
+								else
+								{
+									var dir = delta.GetNormalized(out var dist);
+
+									GUI.DrawLine(c_pos_mouse, c_pos_target, color: GUI.col_button_yellow.WithAlpha(100), thickness: 4.00f);
+								}
+
 								//GUI.DrawLine()
-								GUI.DrawRectFilled(region.WorldToCanvas(AABB.Centered(this.controllable.target_pos.SnapFloor(0.125f), new Vector2(0.250f))), Color32BGRA.Orange);
-								GUI.DrawRectFilled(region.WorldToCanvas(AABB.Centered(this.control.mouse.position.SnapFloor(0.125f), new Vector2(0.250f))), Color32BGRA.Orange);
+								GUI.DrawRectFilled(region.WorldToCanvas(AABB.Centered(this.controllable.target_pos, new Vector2(0.250f))), Color32BGRA.Orange);
+								GUI.DrawRectFilled(region.WorldToCanvas(AABB.Centered(this.control.mouse.position, new Vector2(0.125f))), Color32BGRA.Green);
 							}
 						}
 					}
