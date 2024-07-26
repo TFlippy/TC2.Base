d93ed45b707bbbf23da27823f1116697450109df
TFlippy <TFlippy@users.noreply.github.com>
2024-07-26 03:53:56 +0200

    Misc

--- content/Entities/Lootable.cs
+++ content/Entities/Lootable.cs
@@ -56,80 +56,84 @@ namespace TC2.Base.Components
 				ref var material = ref resource.material.GetData();
 				if (material.IsNotNull() && material.conversions != null && material.conversions.TryGetValue(data.damage_type, out var conv) && random.NextBool(conv.chance))
 				{
+					var has_no_offset = breakable.flags.HasAny(Breakable.Flags.No_Offset) | conv.spawn_flags.HasAny(Resource.SpawnFlags.No_Offset);
+					var has_no_mass_conversion = breakable.flags.HasAny(Breakable.Flags.No_Mass_Conversion) | conv.flags.HasAny(IMaterial.Conversion.Flags.No_Mass_Conversion);
+
+					var amount = Maths.Min(resource.quantity, MathF.Ceiling(resource.quantity * amount_multiplier));
+					var amount_rem = amount;
+
+					var yield = Maths.Clamp01(conv.yield * data.yield);
+
+					//ref var material_conv = ref conv.h_material.GetData();
+					//if (material_conv.IsNotNull())
+					//{
+					var conv_ratio = random.NextFloatExtra(conv.ratio, conv.ratio_extra).Clamp01();
+					var waste_ratio = random.NextFloatExtra(conv.ratio_waste, conv.ratio_waste_extra).Clamp01();
+
+					//App.WriteLine($"amount: {amount}; conv_ratio: {conv_ratio}");
+					var amount_converted = amount_rem.Split(conv_ratio);
+					var amount_wasted = amount_rem.Split(waste_ratio);
+
+
+					//amount_taken.Split(data.yield * conv.yield, out var amount_wasted, out var amount_converted);
+
+
+
+					//App.WriteLine($"taken: {amount_taken}; wasted: {amount_wasted}; converted: {amount_converted}; rem: {amount_rem}; yield: {yield}");
+
+					//var amount_wasted = amount_rem.MultDiff(conv.yield);
+					//App.WriteLine($"amount_wasted: {amount_wasted}; {amount_rem}");
+
+					//amount_taken -= amount_converted;
+					//amount_taken *= conv.yield;
+
+					//resource.quantity -= amount_converted;
+					//var amount_converted_corrected = amount_converted * (material.mass_per_unit / material_conv.mass_per_unit);
+
 					ref var material_conv = ref conv.h_material.GetData();
 					if (material_conv.IsNotNull())
 					{
-						var has_no_offset = breakable.flags.HasAny(Breakable.Flags.No_Offset) | conv.spawn_flags.HasAny(Resource.SpawnFlags.No_Offset);
-						var has_no_mass_conversion = breakable.flags.HasAny(Breakable.Flags.No_Mass_Conversion) | conv.flags.HasAny(IMaterial.Conversion.Flags.No_Mass_Conversion);
-
-						var amount = Maths.Min(resource.quantity, MathF.Ceiling(resource.quantity * amount_multiplier));
-						var amount_rem = amount;
-
-						var conv_ratio = random.NextFloatExtra(conv.ratio, conv.ratio_extra).Clamp01();
-						var waste_ratio = random.NextFloatExtra(conv.ratio_waste, conv.ratio_waste_extra).Clamp01();
-
-						var yield = Maths.Clamp01(conv.yield * data.yield);
-
-						//App.WriteLine($"amount: {amount}; conv_ratio: {conv_ratio}");
-						var amount_converted = amount_rem.Split(conv_ratio);
-						var amount_wasted = amount_rem.Split(waste_ratio);
-						amount_rem *= yield;
-
-						//amount_taken.Split(data.yield * conv.yield, out var amount_wasted, out var amount_converted);
-
-
-						var amount_taken = amount - amount_rem;
-						//App.WriteLine($"taken: {amount_taken}; wasted: {amount_wasted}; converted: {amount_converted}; rem: {amount_rem}; yield: {yield}");
-
-						//var amount_wasted = amount_rem.MultDiff(conv.yield);
-						//App.WriteLine($"amount_wasted: {amount_wasted}; {amount_rem}");
-
-						//amount_taken -= amount_converted;
-						//amount_taken *= conv.yield;
-
-						//resource.quantity -= amount_converted;
-						//var amount_converted_corrected = amount_converted * (material.mass_per_unit / material_conv.mass_per_unit);
-
-						{
-							var spawn_flags_conv = spawn_flags | conv.spawn_flags;
-							Resource.Spawn(region: ref region,
-							material: conv.h_material,
-							world_position: has_no_offset ? body.GetPosition() : data.world_position,
-							amount: has_no_mass_conversion ? amount_converted : Resource.GetConvertedQuantity(resource.material, conv.h_material, amount_converted),
-							max_distance: 4.00f,
-							flags: spawn_flags_conv,
-							ent_target: ent_attacker,
-							ent_owner: ent_owner,
-							angular_velocity: body.GetAngularVelocity(),
-							velocity: has_no_offset ? body.GetVelocity() : body.GetVelocity() + random.NextUnitVector2Range(0, 4));
-						}
-
-						ref var material_waste = ref conv.h_material_waste.GetData();
-						if (material_waste.IsNotNull())
-						{
-							var spawn_flags_conv = spawn_flags | conv.spawn_flags_waste;
-							Resource.Spawn(region: ref region,
-							material: conv.h_material_waste,
-							world_position: has_no_offset ? body.GetPosition() : data.world_position,
-							amount: has_no_mass_conversion ? amount_wasted : Resource.GetConvertedQuantity(resource.material, conv.h_material_waste, amount_wasted),
-							max_distance: 4.00f,
-							flags: spawn_flags_conv,
-							ent_target: ent_attacker,
-							ent_owner: ent_owner,
-							angular_velocity: body.GetAngularVelocity(),
-							velocity: has_no_offset ? body.GetVelocity() : body.GetVelocity() + random.NextUnitVector2Range(0, 4));
-						}
-
-						if (conv.h_sound.id != 0)
-						{
-							Sound.Play(ref region, conv.h_sound, data.world_position, volume: conv.sound_volume * random.NextFloatExtra(0.90f, 0.20f), pitch: conv.sound_pitch * random.NextFloatExtra(0.95f, 0.10f));
-						}
-
-						//var amount_taken = amount - amount_rem;
-						resource.quantity -= amount_taken;
-
-						resource.Modified(entity, true);
+						var spawn_flags_conv = spawn_flags | conv.spawn_flags;
+						Resource.Spawn(region: ref region,
+						material: conv.h_material,
+						world_position: has_no_offset ? body.GetPosition() : data.world_position,
+						amount: has_no_mass_conversion ? amount_converted : Resource.GetConvertedQuantity(resource.material, conv.h_material, amount_converted),
+						max_distance: 4.00f,
+						flags: spawn_flags_conv,
+						ent_target: ent_attacker,
+						ent_owner: ent_owner,
+						angular_velocity: body.GetAngularVelocity(),
+						velocity: has_no_offset ? body.GetVelocity() : body.GetVelocity() + random.NextUnitVector2Range(0, 4));
 					}
+
+					ref var material_waste = ref conv.h_material_waste.GetData();
+					if (material_waste.IsNotNull())
+					{
+						var spawn_flags_conv = spawn_flags | conv.spawn_flags_waste;
+						Resource.Spawn(region: ref region,
+						material: conv.h_material_waste,
+						world_position: has_no_offset ? body.GetPosition() : data.world_position,
+						amount: has_no_mass_conversion ? amount_wasted : Resource.GetConvertedQuantity(resource.material, conv.h_material_waste, amount_wasted),
+						max_distance: 4.00f,
+						flags: spawn_flags_conv,
+						ent_target: ent_attacker,
+						ent_owner: ent_owner,
+						angular_velocity: body.GetAngularVelocity(),
+						velocity: has_no_offset ? body.GetVelocity() : body.GetVelocity() + random.NextUnitVector2Range(0, 4));
+					}
+					//var amount_taken = amount - amount_rem;
+					//}
+
+					if (conv.h_sound.id != 0)
+					{
+						Sound.Play(ref region, conv.h_sound, data.world_position, volume: conv.sound_volume * random.NextFloatExtra(0.90f, 0.20f), pitch: conv.sound_pitch * random.NextFloatExtra(0.95f, 0.10f));
+					}
+
+					amount_rem *= yield;
+
+					var amount_taken = amount - amount_rem;
+					resource.quantity -= amount_taken;
+					resource.Modified(entity, true);
 				}
 			}
 #endif
