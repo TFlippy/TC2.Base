5a4ae5a3a24361739fa1ff720aa7b6d68f70b6e9
TFlippy <TFlippy@users.noreply.github.com>
2025-03-08 03:35:51 +0100

    Chisel stuff

--- content/Entities/Tools/Chisel/Chisel.cs
+++ content/Entities/Tools/Chisel/Chisel.cs
@@ -22,11 +22,56 @@
 		public struct UseRPC: Net.IRPC<Chisel.Data>
 		{
 			public Vector2 world_position;
-			public Vector2 normal;
+			public Vector2 direction;
 
 #if SERVER
 			public void Invoke(Net.IRPC.Context rpc, ref Chisel.Data data)
 			{
+				ref var region = ref rpc.GetRegion();
+				ref var terrain = ref region.GetTerrain();
+
+				var rect = Terrain.GetGridRect(this.world_position, new Vector2(0.125f));
+
+				//var size = new Vector2(1.00f);
+				//var size_half = size * 0.50f;
+
+				//var w_pos_new = this.world_position;
+				//var w_pos_new_snapped = w_pos_new.SnapFloor(0.125f);
+				//var bb = AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
+				//var bb_outside = bb.Grow(0.50f); // AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
+
+				//var color = Color32BGRA.Yellow;
+				//var color_ok = Color32BGRA.Green;
+				//var c_pos_new_snapped = region.WorldToCanvas(w_pos_new_snapped);
+
+
+				//var bb = AABB.Simple(w_pos_new_snapped - size_half, size + new Vector2(0.125f));
+
+				var tile_flags = TileFlags.Non_Empty;
+				//var pixel_size = new Vector2(App.pixels_per_unit_inv) * region.GetWorldToCanvasScale();
+
+				//GUI.DrawChunkRect(ref region, w_pos_new);
+
+				terrain.Hit(rpc.entity, rpc.entity, rect: rect, world_position: this.world_position, direction: this.direction, damage: 100000, yield: 0.00f, mask: TileFlags.Non_Empty, no_loot_pickup: true);
+				//region.DrawDebugRect(rect, Color32BGRA.Magenta);
+
+				//{
+				//	var args = new DrawTileArgs(offset: region.WorldToCanvas(bb_outside.a), pixel_size: pixel_size, color: color.WithAlpha(100), tile_flags: tile_flags);
+				//	terrain.IterateRect(bb_outside, argument: ref args,
+				//		func: DrawTileFunc,
+				//		iteration_flags: Terrain.IterationFlags.Iterate_Empty);
+				//}
+
+				//GUI.DrawTerrainOutline(ref region, w_pos_new, radius: 1.00f, thickness: 1.00f, color: GUI.col_button_yellow.WithAlpha(200));
+
+				//{
+				//	var args = new DrawTileArgs(offset: region.WorldToCanvas(bb.a), pixel_size: pixel_size, color: color_ok.WithAlpha(200), tile_flags: tile_flags);
+				//	terrain.IterateRect(bb, argument: ref args,
+				//		func: DrawTileFunc,
+				//		iteration_flags: Terrain.IterationFlags.Iterate_Empty);
+				//}
+
+
 				//var sync = false;
 
 				//var positions_span = data.positions.Slice(data.positions_capacity);
@@ -83,19 +128,20 @@
 
 						Arm.HoverGUI.Hide();
 
+						var size = 0.50f; // new Vector2(0.50f);
+						var size_half = size * 0.50f;
+
 						var w_pos_new = mouse.position - new Vector2(0.25f);
 						var w_pos_new_snapped = w_pos_new.SnapFloor(0.125f);
+						var bb = AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
+						var bb_outside = bb.Grow(0.50f); // AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
 
 						var color = Color32BGRA.Yellow;
 						var color_ok = Color32BGRA.Green;
 						var c_pos_new_snapped = region.WorldToCanvas(w_pos_new_snapped);
 
-						var size = new Vector2(0.50f);
-						var size_half = size * 0.50f;
 
 						//var bb = AABB.Simple(w_pos_new_snapped - size_half, size + new Vector2(0.125f));
-						var bb = AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
-						var bb_outside = bb.Grow(0.50f); // AABB.Simple(w_pos_new_snapped, new Vector2(0.125f));
 
 						var tile_flags = TileFlags.Non_Empty;
 						var pixel_size = new Vector2(App.pixels_per_unit_inv) * region.GetWorldToCanvasScale();
@@ -122,33 +168,32 @@
 						GUI.DrawCross(region.WorldToCanvas(w_pos_new_snapped + new Vector2(0.000f)), Color32BGRA.White.WithAlpha(50), radius: region.GetWorldToCanvasScale() * 1.000f);
 						GUI.DrawCross(region.WorldToCanvas(w_pos_new_snapped + new Vector2(0.125f)), Color32BGRA.White.WithAlpha(50), radius: region.GetWorldToCanvasScale() * 1.000f);
 
-						//	if (!GUI.IsHovered)
-						//	{
-						//		if (mouse.GetKeyDown(Mouse.Key.Left))
-						//		{
-						//			var rpc = new Chisel.MarkRPC()
-						//			{
-						//				index = (byte)positions_count,
-						//				position = w_pos_new_snapped,
-						//			};
-						//			rpc.Send(this.entity);
-						//		}
-						//		else if (mouse.GetKeyDown(Mouse.Key.Right))
-						//		{
-						//			if (nearest_index >= 0)
-						//			{
-						//				var rpc = new Chisel.MarkRPC()
-						//				{
-						//					index = (byte)nearest_index,
-						//					position = null,
-						//				};
-						//				rpc.Send(this.entity);
-						//			}
-						//		}
-						//	}
-						//}
+						if (!GUI.IsHovered)
+						{
+							if (mouse.GetKeyDown(Mouse.Key.Left))
+							{
+								var rpc = new Chisel.UseRPC()
+								{
+									world_position = w_pos_new
+								};
+								rpc.Send(this.entity);
+							}
+							//else if (mouse.GetKeyDown(Mouse.Key.Right))
+							//{
+							//	if (nearest_index >= 0)
+							//	{
+							//		var rpc = new Chisel.MarkRPC()
+							//		{
+							//			index = (byte)nearest_index,
+							//			position = null,
+							//		};
+							//		rpc.Send(this.entity);
+							//	}
+							//}
+						}
 					}
 				}
+				
 			}
 
 			private record struct DrawTileArgs(Vector2 offset, Vector2 pixel_size, TileFlags tile_flags, Color32BGRA color);
