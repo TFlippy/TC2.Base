7f26ccdd5264f88c8e90d9cb1e37f32b85a39ef9
TFlippy <TFlippy@users.noreply.github.com>
2025-08-29 07:55:39 +0200

    Add spin torque and speed to creature arms, update pump and recipes
    Introduced 'spin_torque' and 'max_spin_speed' parameters to the arm definitions of Giant, Human, and Kobold creatures for both male and female variants. Refactored Pump.cs to merge State into Data, updated network send type, and improved water resource deposit logic. Adjusted interactable window properties for track structures and increased miss chance for wreck crate bodies. Updated chemistry and item recipes with new version numbers, hints, connotations, and company fields.

--- content/Entities/Structures/Industry/Pump/Pump.cs
+++ content/Entities/Structures/Industry/Pump/Pump.cs
@@ -2,17 +2,15 @@
 {
 	public static partial class Pump
 	{
-		[IComponent.Data(Net.SendType.Reliable, IComponent.Scope.Region), IComponent.With<Pump.State>]
+		[IComponent.Data(Net.SendType.Unreliable, IComponent.Scope.Region)]
 		public struct Data(): IComponent
 		{
-			[Editor.Picker.Position(true)] public Vector2 slider_offset;
+			[Editor.Picker.Position(true)] public Vec2f slider_offset;
 			public float volume_speed_mult = 0.30f;
 			public Volume volume = 0.70f;
-		}
 
-		[IComponent.Data(Net.SendType.Unreliable, IComponent.Scope.Region)]
-		public struct State(): IComponent
-		{
+			public IMaterial.Handle h_material_water;
+
 			public Pressure pressure;
 			public float interval = 1.00f;
 			[Save.Ignore, Net.Ignore] public float next_update;
@@ -22,7 +20,8 @@
 		[ISystem.LateUpdate(ISystem.Mode.Single, ISystem.Scope.Region)]
 		public static void OnUpdateFX(ISystem.Info info, 
 		[Source.Owned] in Transform.Data transform, [Source.Owned] in Pump.Data pump,
-		[Source.Owned] ref Sound.Emitter sound_emitter, [Source.Owned] in Axle.Data axle, [Source.Owned] in Axle.State axle_state, 
+		[Source.Owned] in Axle.Data axle, [Source.Owned] in Axle.State axle_state,
+		[Source.Owned, Pair.Component<Pump.Data>] ref Sound.Emitter sound_emitter,
 		[Source.Owned, Pair.Component<Pump.Data>] ref Animated.Renderer.Data renderer_slider)
 		{
 			var speed = MathF.Abs(axle_state.angular_velocity);
@@ -38,11 +37,15 @@
 		[ISystem.Update.C(ISystem.Mode.Single, ISystem.Scope.Region)]
 		public static void OnUpdate(ISystem.Info info, ref XorRandom random, 
 		[Source.Owned] in Transform.Data transform, 
-		[Source.Owned] in Pump.Data pump, [Source.Owned] ref Pump.State pump_state,
+		[Source.Owned] in Pump.Data pump,
 		[Source.Owned] in Axle.Data axle, [Source.Owned] ref Axle.State axle_state, 
 		[Source.Owned, Pair.Component<Pump.Data>] ref Inventory1.Data inventory)
 		{
-
+			if (axle_state.flags.HasAny(Axle.State.Flags.Revolved))
+			{
+				var resource = new Resource.Data(pump.h_material_water, random.NextFloatExtra(0.50f, 1.00f));
+				inventory.Deposit(ref resource);
+			}
 		}
 #endif
 	}
