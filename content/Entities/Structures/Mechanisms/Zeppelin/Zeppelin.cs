e0f01b59b7b8f6ff9686177419d1cf5944f71daf
TFlippy <TFlippy@users.noreply.github.com>
2026-02-02 06:32:17 +0100

    Zeppelin (WIP)

---
+++ content/Entities/Structures/Mechanisms/Zeppelin/Zeppelin.cs
@@ -0,0 +1,58 @@
+﻿namespace TC2.Base.Components
+{
+	public static partial class Zeppelin
+	{
+		[IComponent.Data(Net.SendType.Reliable, IComponent.Scope.Region | IComponent.Scope.Global)]
+		public partial struct Data(): IComponent
+		{
+			public float unused_00;
+			public float unused_01;
+			public float unused_02;
+			public float unused_03;
+		}
+
+#if CLIENT
+		public partial struct ZeppelinGUI: IGUICommand
+		{
+			public Entity ent_zeppelin;
+			public Zeppelin.Data zeppelin;
+			public Transform.Data transform;
+
+			public void Draw()
+			{
+				using (var window = GUI.Window.Interaction("Zeppelin"u8, this.ent_zeppelin))
+				{
+					this.StoreCurrentWindowTypeID(order: 6);
+					if (window.show)
+					{
+
+					}
+				}
+			}
+		}
+
+		[ISystem.GUI(ISystem.Mode.Single, ISystem.Scope.Region)]
+		public static void OnGUI([Source.Owned] in Interactable.Data interactable, 
+		Entity ent_zeppelin, [Source.Owned] in Zeppelin.Data zeppelin, [Source.Owned] in Transform.Data transform)
+		{
+			if (interactable.IsActive())
+			{
+				var gui = new ZeppelinGUI()
+				{
+					ent_zeppelin = ent_zeppelin,
+					zeppelin = zeppelin,
+					transform = transform,
+				};
+				gui.Submit();
+			}
+		}
+#endif
+
+		[ISystem.Update.C(ISystem.Mode.Single, ISystem.Scope.Region)]
+		public static void OnUpdate(ISystem.Info info, ref Region.Data region, Entity entity,
+		[Source.Owned] ref Zeppelin.Data zeppelin, [Source.Owned] ref Transform.Data transform, [Source.Owned] ref Body.Data body)
+		{
+
+		}
+	}
+}
